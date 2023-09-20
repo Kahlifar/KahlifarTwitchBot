668ca7c4108776c69ae31caec1a901346bb38214
Fynn Westerath <westerathf@gmail.com>
2023-09-20 07:20:57 +0200

    update Timer class, add rules timer

--- timers/socials.ts
+++ timers/socials.ts
@@ -4,6 +4,7 @@ import { TmiClient } from '../types/TmiClient';
 module.exports = new Timer(
     'socials',
     5 * 60,
+    2.5 * 60,
     (client: TmiClient) => {
         console.log('socials');
         client.say(client.config.channel, client.localizator.__('commandSocials'));
--- types/Timer.ts
+++ types/Timer.ts
@@ -4,18 +4,22 @@ import Timeout = NodeJS.Timeout;
 export class Timer {
     name: string;
     interval: number;
+    offset: number = 0;
     callback: Function;
 
     intervalId: Timeout | number | null = null;
 
-    constructor(name: string, interval: number, callback: Function) {
+    constructor(name: string, interval: number, offset: number, callback: Function) {
         this.name = name;
         this.interval = interval;
+        this.offset = offset;
         this.callback = callback;
     }
 
     start(client: TmiClient) {
-        this.intervalId = setInterval(() => this.callback(client), this.interval * 1000);
+        setTimeout(() => {
+            this.intervalId = setInterval(() => this.callback(client), this.interval * 1000);
+        }, this.offset * 1000);
     }
 
     stop() {
